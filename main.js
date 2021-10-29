237cfc011cfb3af27f81bbd7a526b8dcd15d7549
oosasa <93368086+oosasa@users.noreply.github.com>
2021-10-29 15:58:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,6 @@
           body = $('<dl>').appendTo(html),
           foot = $('<dl>').appendTo(html);
     const addCSS = href => $('<link>').appendTo('head').prop({href, rel: 'stylesheet'});
-    addCSS('https://unpkg.com/nes.css@2.2.0/css/nes.min.css');
     const makeTable = (w, h) => {
         const table = $('<table>');
         for(const y of Array(h).keys()) {
@@ -30,17 +29,22 @@
                   'table-layout': 'fixed',
                   margin: 'auto'
               });
-        const t = table.find('td');
-        const w = addBtn(t.get(toI(0, 1)), '←', () => move('←')).outerWidth();
-        addBtn(t.get(toI(2, 1)), '→', () => move('→'));
-        addBtn(t.get(toI(1, 0)), '↑', () => move('↑'));
-        addBtn(t.get(toI(1, 2)), '↓', () => move('↓'));
-        addBtn(t.get(toI(0 + 4, 1)), 'Y', () => move('Y'));
-        addBtn(t.get(toI(2 + 4, 1)), 'A', () => move('A'));
-        addBtn(t.get(toI(1 + 4, 0)), 'X', () => move('X'));
-        addBtn(t.get(toI(1 + 4, 2)), 'B', () => move('B'));
-        console.log(w)
-        table.css({width: w * width});
+        const td = table.find('td'),
+              _a = addBtn(td.get(toI(0, 1)), '←', () => move('←')),
+              _d = addBtn(td.get(toI(2, 1)), '→', () => move('→')),
+              _w = addBtn(td.get(toI(1, 0)), '↑', () => move('↑')),
+              _s = addBtn(td.get(toI(1, 2)), '↓', () => move('↓')),
+              y = addBtn(td.get(toI(0 + 4, 1)), 'Y', () => move('Y')),
+              a = addBtn(td.get(toI(2 + 4, 1)), 'A', () => move('A')),
+              x = addBtn(td.get(toI(1 + 4, 0)), 'X', () => move('X')),
+              b = addBtn(td.get(toI(1 + 4, 2)), 'B', () => move('B'));
+        _a.add(_d).add(_w).add(_s).add(td.get(toI(1, 1))).css({
+            'backgroundColor': 'rgba(66, 86, 123, 0.5)'
+        });
+        y.add(a).add(x).add(b).css({
+            'border-radius': '90%'
+        });
+        table.css({width: _a.outerWidth() * width});
     }
     const move = c => {
         $('<div>').prependTo(foot).text(c + ' ' + new Date);
